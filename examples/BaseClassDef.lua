9adec80daa475923fb23087c8b344277f271c835
swordday <swordday2008@gmail.com>
2018-04-25 21:45:17 +0800

    update

--- examples/BaseClassDef.lua
+++ examples/BaseClassDef.lua
@@ -3,14 +3,15 @@ local class = require("rtti").class
 BaseClass = BaseClass or class("BaseClass")
 
 function BaseClass:ctor()
+    -- private variable initialized    
+    local this = self.__pdatas
+    -- variable x read/write    
+    this.m_x = 0
+    -- variable y only read
+    this.m_y = 0
+
     -- public variable initialized
     self.m_value = 1
-
-    -- private variable initialized    
-    -- variable x read/write
-    self.__pdatas.m_x = 0
-    -- variable y only read
-    self.__pdatas.m_y = 0
 end
 
 function BaseClass:get_x()
