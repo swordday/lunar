5b6cc6a029369975c49ee0d99d0720c039460f1f
swordday <swordday2008@gmail.com>
2018-04-26 17:07:28 +0800

    add containers/array

---
+++ lunar/containers/array.lua
@@ -0,0 +1,67 @@
+local table_insert = table.insert
+local table_remove = table.remove
+
+
+local class = require("rtti").class
+
+local array = class("array")
+
+-- Basic
+function array:ctor()
+    local this = self.__pdatas
+    this.m_size = 0
+    this.m_buffer = {}
+end
+
+-- Functional
+function array:Add(index, value)
+    local this = self.__pdatas
+
+    this.m_buffer[index] = value
+    table_insert(this.m_buffer, index, value)
+    this.m_size = this.m_size + 1
+end
+
+function array:Remove(index)
+    local this = self.__pdatas
+
+    table_remove(this.m_buffer, index)
+    this.m_size = this.m_size - 1
+end
+
+function array:PushBack(value)
+    local this = self.__pdatas
+
+    this.m_size = this.m_size + 1
+    this.m_buffer[this.m_size] = value
+end
+
+function array:PopBack()
+    local this = self.__pdatas
+
+    local value = this.m_buffer[this.m_size]
+    
+    this.m_buffer[this.m_size] = nil
+    this.m_size = this.m_size - 1
+
+    return value
+end
+
+function array:Empty()
+    return self:Size() == 0
+end
+
+function array:Size()
+    return self.__pdatas.m_size
+end
+
+-- Getter and Setter
+function array:get(index)
+    return self.__pdatas.m_buffer[index]
+end
+
+function array:set(index, value)
+    self.__pdatas.m_buffer[index] = value
+end
+
+return array
